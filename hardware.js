db87aa6a2265cfd5e62c291cab12f96879fd6eff
Abhi <abhijit.das3002@gmail.com>
2024-01-08 22:45:29 -0500

    sketch working

--- hardware.js
+++ hardware.js
@@ -4,17 +4,20 @@ let width = 600;
 let cellSize = width / rowLength;
 
 const grid = document.getElementById('grid');
+
 for (let i = 0; i < rowLength; i++) {
-    
+    const row = document.createElement("div");
+    row.className = 'row';
     for (let j = 0; j < rowLength; j++) {
         const cell = document.createElement("div");
         cell.className = 'cell';
         cell.style.width = `${cellSize}px`;
         cell.style.height = `${cellSize}px`;
-        cell.style.backgroundColor = 'blue';
-        grid.appendChild(cell);
+        cell.addEventListener('mouseover', () => {
+            cell.style.opacity = +cell.style.opacity + 0.1;
+            console.log(cell.style.opacity);
+        });
+        row.appendChild(cell);
     }
+    grid.appendChild(row);
 }
-
-
-

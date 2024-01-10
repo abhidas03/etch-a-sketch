ad7864c3b01bda885d5d6a2788c2f3aa67b2cb77
Abhi <abhijit.das3002@gmail.com>
2024-01-09 19:27:51 -0500

    slider functioning

--- hardware.js
+++ hardware.js
@@ -1,28 +1,35 @@
+function createGrid(width, rowLength) {
+    cellSize = width / rowLength; 
+    for (let i = 0; i < rowLength; i++) {
+        const row = document.createElement("div");
+        row.className = 'row';
+        for (let j = 0; j < rowLength; j++) {
+            const cell = document.createElement("div");
+            cell.className = 'cell';
+            cell.style.width = `${cellSize}px`;
+            cell.style.height = `${cellSize}px`;
+            cell.addEventListener('mouseover', () => {
+                cell.style.opacity = +cell.style.opacity + 0.1;
+            });
+            row.appendChild(cell);
+        }
+        grid.appendChild(row);
+    }
+}
+
 const slider = document.getElementById('slider');
 const grid = document.getElementById('grid');
 const sliderText = document.getElementById('slider_text');
 
-sliderText.innerHTML = slider.value;
+sliderText.innerHTML = `${slider.value} x ${slider.value}`;
 let rowLength = slider.value;
 
 let width = 600;
 let cellSize = width / rowLength;
+createGrid(width, rowLength);
 slider.oninput = () => {
-    sliderText.innerHTML = slider.value;
+    sliderText.innerHTML = `${slider.value} x ${slider.value}`;
     rowLength = slider.value;
-}
-for (let i = 0; i < rowLength; i++) {
-    const row = document.createElement("div");
-    row.className = 'row';
-    for (let j = 0; j < rowLength; j++) {
-        const cell = document.createElement("div");
-        cell.className = 'cell';
-        cell.style.width = `${cellSize}px`;
-        cell.style.height = `${cellSize}px`;
-        cell.addEventListener('mouseover', () => {
-            cell.style.opacity = +cell.style.opacity + 0.1;
-        });
-        row.appendChild(cell);
-    }
-    grid.appendChild(row);
+    grid.replaceChildren();
+    createGrid(width, rowLength);
 }

44316e6c6e3bc0dd23ed8ff56241d1f9de9af64a
Abhi <abhijit.das3002@gmail.com>
2024-02-12 23:28:30 -0500

    Added rainbow colors

--- hardware.js
+++ hardware.js
@@ -8,19 +8,39 @@ function createGrid(width, rowLength) {
             cell.className = 'cell';
             cell.style.width = `${cellSize}px`;
             cell.style.height = `${cellSize}px`;
-            cell.addEventListener('mouseover', () => {
-                cell.style.opacity = +cell.style.opacity + 0.1;
-            });
+            if (color_mode == 0) {
+                cell.style.opacity = 0;
+                cell.addEventListener('mouseover', () => {
+                    cell.style.opacity = +cell.style.opacity + 0.1;
+                });
+            }
+            else if (color_mode == 1) {
+                cell.style.opacity = 1;
+                cell.addEventListener('mouseover', () => {
+                    let color = getRandomColor();
+                    console.log(color);
+                    cell.style.backgroundColor = color;
+                });
+            }
             row.appendChild(cell);
         }
         grid.appendChild(row);
     }
 }
 
+function getRandomColor() {
+    const colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'];
+    const rand = Math.floor(Math.random() * 7);
+    return colors[rand];
+}
+
 const slider = document.getElementById('slider');
 const grid = document.getElementById('grid');
 const sliderText = document.getElementById('slider_text');
+const grayscale_btn = document.getElementById('grayscale');
+const rainbow_btn = document.getElementById('rainbow');
 
+let color_mode = 0;
 sliderText.innerHTML = `${slider.value} x ${slider.value}`;
 let rowLength = slider.value;
 
@@ -31,5 +51,20 @@ slider.oninput = () => {
     sliderText.innerHTML = `${slider.value} x ${slider.value}`;
     rowLength = slider.value;
     grid.replaceChildren();
-    createGrid(width, rowLength);
+    createGrid(width, rowLength, color_mode);
 }
+
+grayscale_btn.onclick = () => {
+    if (color_mode == 0) return;
+    color_mode = 0;
+    grid.replaceChildren();
+    createGrid(width, rowLength, color_mode);
+}
+rainbow_btn.onclick = () => {
+    if (color_mode == 1) return;
+    color_mode = 1;
+    grid.replaceChildren();
+    createGrid(width, rowLength, color_mode);
+}
+
+
